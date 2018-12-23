d9ad7630882426938b0b86c13d5f82ff768cb300
Emily Horsman <me@emilyhorsman.com>
2018-12-23 16:59:21 -0500

    Add from_parameter test
    * Chapter 5 of The Ray Tracer Challenge

--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,4 @@
 use crate::vector::*;
 
+#[derive(Debug, PartialEq)]
 pub struct Point(pub Vec3f);
--- src/ray.rs
+++ src/ray.rs
@@ -12,3 +12,42 @@ impl Ray {
         Point(o + self.direction * t)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Ray;
+    use crate::point::Point;
+    use crate::vector::Vec3f;
+
+    #[test]
+    fn it_computes_ray_from_parameter_t() {
+        let r = Ray {
+            origin: Point(Vec3f {
+                x: 2.0,
+                y: 3.0,
+                z: 4.0,
+            }),
+            direction: Vec3f {
+                x: 1.0,
+                y: 0.0,
+                z: 0.0,
+            },
+        };
+        assert_eq!(
+            r.from_parameter(0.0),
+            Point(Vec3f {
+                x: 2.0,
+                y: 3.0,
+                z: 4.0
+            })
+        );
+        assert_eq!(
+            r.from_parameter(-1.0),
+            Point(Vec3f {
+                x: 1.0,
+                y: 3.0,
+                z: 4.0
+            })
+        );
+    }
+}
