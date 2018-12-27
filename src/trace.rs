9871984ab7bc5f1c21e719e6587fbedbde5a6c40
Emily Horsman <me@emilyhorsman.com>
2018-12-26 19:19:43 -0500

    Refactor out the reflection computation

--- src/trace.rs
+++ src/trace.rs
@@ -21,14 +21,35 @@ pub fn lighting(
     }
 
     let diffuse = effective_color * material.diffuse * facing_ratio;
-    let reflection_direction = reflect(&(-shadow_direction), normal);
-    let eye = -incoming_ray.direction;
-    let reflection_ratio = reflection_direction.dot(&eye);
-    let mut specular = Color::new(0.0, 0.0, 0.0);
-    if reflection_ratio > 0.0 {
-        let factor = reflection_ratio.powf(material.shininess);
-        specular = light.color * material.specular * factor;
-    }
+    let specular = compute_reflection(material, light, incoming_ray, normal, &shadow_direction);
 
     (ambient + diffuse + specular).clamp()
+}
+
+/// Returns None if the shadow ray is being reflected away from the eye. Otherwise this
+/// returns the cosine of the angle between the reflection vector and eye vector.
+fn reflection_ratio(incoming_ray: &Ray, normal: &Vec3f, shadow_direction: &Vec3f) -> Option<Float> {
+    let reflection_direction = reflect(&(-shadow_direction), normal);
+    let ratio = reflection_direction.dot(&(-incoming_ray.direction));
+    if ratio <= 0.0 {
+        None
+    } else {
+        Some(ratio)
+    }
+}
+
+fn compute_reflection(
+    material: &Material,
+    light: &PointLight,
+    incoming_ray: &Ray,
+    normal: &Vec3f,
+    shadow_direction: &Vec3f,
+) -> Color {
+    let ratio = match reflection_ratio(incoming_ray, normal, shadow_direction) {
+        Some(r) => r,
+        None => return Color::new(0.0, 0.0, 0.0),
+    };
+
+    let factor = ratio.powf(material.shininess);
+    light.color * material.specular * factor
 }
