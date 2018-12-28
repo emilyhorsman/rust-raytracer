9752f419be7df3b30f740fb07011f74b70340a49
Emily Horsman <me@emilyhorsman.com>
2018-12-27 19:34:37 -0500

    Pass view transformation tests
    * From Chapter 7 of The Ray Tracing Challenge

--- src/view_transformation.rs
+++ src/view_transformation.rs
@@ -20,13 +20,28 @@ impl Default for ViewTransformation {
 
 impl ViewTransformation {
     pub fn matrix(&self) -> Projective3<Float> {
-        Projective3::identity()
+        let forward = (self.to - self.from).normalize();
+        let up = self.up.normalize();
+        let left = forward.cross(&up);
+        // The given up vector does not need to be perfectly orthogonal.
+        let true_up = left.cross(&forward);
+        let mut orientation = Matrix4::identity();
+        // TODO: Cleanup.
+        orientation.set_row(0, &left.transpose().insert_column(3, 0.0));
+        orientation.set_row(1, &true_up.transpose().insert_column(3, 0.0));
+        orientation.set_row(2, &(-forward).transpose().insert_column(3, 0.0));
+
+        let from_translation = (Point3::new(0.0, 0.0, 0.0) - self.from).insert_row(3, 1.0);
+        let mut translation = Matrix4::identity();
+        translation.set_column(3, &from_translation);
+
+        Projective3::from_matrix_unchecked(orientation * translation)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use approx::assert_relative_eq;
+    use approx::*;
 
     use super::*;
 
@@ -42,9 +57,10 @@ mod tests {
         let m = ViewTransformation {
             to: Point3::new(0.0, 0.0, 1.0),
             ..Default::default()
-        }.matrix();
+        }
+        .matrix();
         let mut correct = Matrix4::identity();
-        correct.set_diagonal(&Vector4::new(1.0, -1.0, 1.0, 1.0));
+        correct.set_diagonal(&Vector4::new(-1.0, 1.0, -1.0, 1.0));
         assert_relative_eq!(*m.matrix(), correct);
     }
 
@@ -56,9 +72,25 @@ mod tests {
             from: Point3::new(0.0, 0.0, 8.0),
             to: Point3::new(0.0, 0.0, 0.0),
             ..Default::default()
-        }.matrix();
+        }
+        .matrix();
         let mut correct = Matrix4::identity();
         correct.set_column(3, &Vector4::new(0.0, 0.0, -8.0, 1.0));
         assert_relative_eq!(*m.matrix(), correct);
+    }
+
+    #[test]
+    fn arbitrary_view_transformation() {
+        let m = ViewTransformation {
+            from: Point3::new(1.0, 3.0, 2.0),
+            to: Point3::new(4.0, -2.0, 8.0),
+            up: Vector3::new(1.0, 1.0, 0.0),
+        }
+        .matrix();
+        let correct = Matrix4::new(
+            -0.50709, 0.50709, 0.67612, -2.36643, 0.76772, 0.60609, 0.12122, -2.82843, -0.35857,
+            0.59761, -0.71714, 0.00000, 0.00000, 0.00000, 0.00000, 1.00000,
+        );
+        assert!(relative_eq!(*m.matrix(), correct, epsilon = 1e-5));
     }
 }
